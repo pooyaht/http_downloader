81bb7c0787f7f032f5f9131cd319c5b806e6b383
pooyaht <pooyahatami83@gmail.com>
2024-03-18 20:00:47 +0330

    Change the order of cli's arguments

--- main.go
+++ main.go
@@ -10,18 +10,18 @@ import (
 func main() {
 	args := os.Args[1:]
 	if len(args) < 3 {
-		fmt.Println("Usage : go run main.go filename port target_server_ip")
+		fmt.Println("Usage : go run main.go target_server_ip port filename")
 		os.Exit(1)
 	}
 
-	filename := args[0]
+	target_server_ip := args[0]
 	port, err := strconv.Atoi(args[1])
 	if err != nil {
 		fmt.Println("Error: ", err)
 		os.Exit(1)
 	}
+	filename := args[2]
 
-	target_server_ip := args[2]
 	server_ip := net.ParseIP(target_server_ip)
 	downloader := NewHttpDownloader(server_ip, port)
 
